2b5378060210ce5f73b067410b0e5e8dbb251327
unknown <sebastian>
2020-08-06 21:47:30 -0600

    es gato me olvide ayuda! wey!!!

---
+++ app/Entity/Cliente.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Entity;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    protected $table = 'Clientes';
+    protected $primaryKey = 'id';
+
+    //
+}
